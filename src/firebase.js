f9c69b0a895295514b3fc4cd38b80bd501eaab6b
Harshitcoding <135004228+Harshitcoding@users.noreply.github.com>
2024-07-01 16:19:24 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyCEMDLIbk5ApnQkot2GEXAIaUpi-o8SkbA",
-  authDomain: "image-uploader-f7126.firebaseapp.com",
-  projectId: "image-uploader-f7126",
-  storageBucket: "image-uploader-f7126.appspot.com",
-  messagingSenderId: "641791161666",
-  appId: "1:641791161666:web:38f0e25b32f0b763ac44db"
+  apiKey: "your apikey",
+  authDomain: "your authDomain",
+  projectId: "your projectId",
+  storageBucket: "your storageBucket",
+  messagingSenderId: "your messagingSenderId",
+  appId: "your appId"
 };
 
 const app = initializeApp(firebaseConfig);
